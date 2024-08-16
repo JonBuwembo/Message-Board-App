7c164a06a5b90b4679ac4ab1c8e4a6ff59171b6f
JonBuwembo <122390422+JonBuwembo@users.noreply.github.com>
2024-08-16 13:21:47 -0400

    Added tests for Heroku deployment

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from .models import Post
+from django.urls import reverse
 
 # Create your tests here.
+
+# Testing the Model
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='Testing: just a simple comment')
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'Testing: just a simple comment')
+
+# Test Hompage --> see if it exists
+class HomePageViewsTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='This is another Test')
+    
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_views_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
+
+
